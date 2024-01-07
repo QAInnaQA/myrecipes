910041a3dbadb2f2d66afb4d2f6b1e8e618bebc7
QAInnaQA <qainnaqa@gmail.com>
2024-01-07 14:22:35 -0800

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import video from "./video.mp4"
+import MyRecipesComponents from './MyRecipesComponents';
 
 function App() {
+
+  const [mySearch, setMySearch] = useState("");
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState("avocado");
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      const response = await fetch('https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=8ab7a4c3&app_key=f14bb474657c53b972835d902e6c962a');
+      const data = await response.json();
+      setMyRecipes(data.hits);
+    }
+    getRecipe()
+  }, [wordSubmitted])
+
+  const myRecipeSearch = (e) => {
+    setMySearch(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const finalSearch = (e) => {
+    e.preventDefaut()
+    setWordSubmitted(mySearch)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'> 
+      <video autoPlay muted loop>
+      <source src={video} type='video/mp4'/>
+      </video>
+      <h1>Find a Recipe</h1>
+      </div>
+
+      <div className='container'>
+        <form onSubmit={finalSearch}>
+          <input className='search' placeholder='search...' onChange={myRecipeSearch} value={mySearch}></input>
+        </form>
+      </div>
+
+      <div className='container'>
+        <button onClick={finalSearch}>
+          <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt='icon'/>
+        </button>
+      </div>
+    
+    {myRecipes.map((element, index) => (
+      <MyRecipesComponents key={index}
+      label={element.recipe.label} 
+      image={element.recipe.image} 
+      calories={element.recipe.calories}
+      ingredients={element.recipe.ingredientLines}/>
+    ))}
     </div>
   );
 }
